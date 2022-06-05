f0713c35770c2445a7ad79ec2a43ad5a95c01f20
andrey545454 <superkurlin2013@yandex.ru>
2022-06-05 08:26:17 +0500

    add urls for VK part

--- rocketeers_bot/urls.py
+++ rocketeers_bot/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('vk_bot/', include('vk_bot.urls')),
     path('admin/', admin.site.urls),
 ]
