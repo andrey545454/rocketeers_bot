729fe66039fa327fd685b870844de78f475ed886
andrey545454 <superkurlin2013@yandex.ru>
2022-06-07 12:35:53 +0500

    refactor & fix buttons

--- vk_bot/vk/vk_api.py
+++ vk_bot/vk/vk_api.py
@@ -28,9 +28,10 @@ class MessageType(str, Enum):
 class CommandType(str, Enum):
     """ List of commands """
     START = 'start'
+    SELECT_PLATFORM = 'select_platform'
 
 
-class ButtonType(str, Enum):
+class PlatformType(str, Enum):
     """ List of platforms """
 
     STEAM = 'steam'
@@ -47,10 +48,10 @@ class Keyboards:
         'buttons': [[
             {'action': {
                 'type': 'text',
-                'payload': f'{{"command": "{button.value}"}}',
-                'label': button.name
+                'payload': f'{{"command": "select_platform", "platform": "{platform.value}"}}',
+                'label': platform.name
             }}
-            for button in ButtonType
+            for platform in PlatformType
         ]]
     })
 
@@ -64,15 +65,29 @@ async def parse_message_obj(obj: dict):
     match payload.get('command'):
         case CommandType.START:
             await vk_send_message(
-                from_id,
-                'Здравствуйте! Чтобы добавить себя выберите платформу',
-                Keyboards.PLATFORM_KEYBOARD
+                to_id=from_id,
+                msg='Чтобы добавить себя выберите платформу',
+                keyboard=Keyboards.PLATFORM_KEYBOARD
             )
+        case CommandType.SELECT_PLATFORM:
+            match payload.get('platform'):
+                case PlatformType.EPIC | PlatformType.PLAYSTATION | PlatformType.XBOX:
+                    await vk_send_message(
+                        from_id,
+                        'Введите свой gameid'
+                    )
+                case PlatformType.STEAM:
+                    await vk_send_message(
+                        from_id,
+                        'Введите ссылку на steam профиль'
+                    )
+                case _:
+                    await vk_send_message(from_id, 'Ошибка! Повторите процесс заного')
         case _:
             await vk_send_message(from_id, 'Ошибка! Повторите процесс заного')
 
 
-async def vk_send_message(to_id: int, msg: str, keyboard: dict = None, payload: str = ''):
+async def vk_send_message(to_id: int, msg: str, keyboard: str = '', payload: str = ''):
 
     async with aiohttp.request(
         'POST',
@@ -88,4 +103,3 @@ async def vk_send_message(to_id: int, msg: str, keyboard: dict = None, payload: 
     ) as resp:
 
         logger.info(f'Send message to {to_id} - response [{resp.status}]')
-        logger.info(f'{await resp.json()}')
